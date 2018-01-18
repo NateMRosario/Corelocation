b34a2d815740f9ac4c500762e533a14f39d2f297
Nathan Rosario <nathanrosario@ac.c4q.nyc>
2018-01-18 13:05:02 -0500

    Initial Commit

--- CoreLocation Test/ViewController.swift
+++ CoreLocation Test/ViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, UISearchBarDelegate, MKMapViewDelegate {
+    
+    var locationManager: CLLocationManager!
+    var annotation: MKAnnotation!
+    var searchController: UISearchController!
+    var localSearchRequest: MKLocalSearchRequest!
+    var localSearch: MKLocalSearch!
+    var localSearchResponse: MKLocalSearchResponse!
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func requestButtonPressed(_ sender: UIBarButtonItem) {
+        print("Tapped")
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Authorized")
+        case .denied, .restricted:
+            print("Denied")
+            guard let validSettingsURL: URL = URL(string: UIApplicationOpenSettingsURLString) else {return}
+            UIApplication.shared.open(validSettingsURL, options: [:], completionHandler: nil)
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        }
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        mapView.mapType = MKMapType.hybridFlyover
+        let searchButton = UIBarButtonItem(barButtonSystemItem:.search, target: self, action: #selector(searchButtonAction))
+        self.navigationItem.rightBarButtonItem = searchButton
+    }
+    @objc func searchButtonAction() {
+        if searchController == nil {
+            searchController = UISearchController(searchResultsController: nil)
+        }
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start {(localSearchResponse, error) -> Void in
+            if localSearchResponse == nil {
+                let alert = UIAlertController(title: "No place found", message: "Try Again", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        determineMyLocation()
+    }
+    func determineMyLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestWhenInUseAuthorization() //Best practice
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        print("User latitude = \(userLocation.coordinate.latitude)")
+        print("User longitude = \(userLocation.coordinate.longitude)")
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let userAnnotation = MKPointAnnotation()
+        userAnnotation.coordinate = userLocation.coordinate
+        userAnnotation.title = "This is us!"
+        mapView.addAnnotation(userAnnotation)
+//        mapView.showsUserLocation = true
+        mapView.setRegion(region, animated: true)
+//        locationManager.stopUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error: \(error)")
     }
-
+    
+    
 
 }
 
